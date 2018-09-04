fce4b9f437102ab5f9f5caa9a40d7ff49f398aad
Ivan <ivan_jr@yahoo.com>
2018-09-03 22:57:24 -0600

    minor correction

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # esp8266-fireplace-starter
 
 ## Main Components
-- ESP8266 (or something similar module)
-- Transmitter (fireplace remote control uses 433Mhz, so I got WRL-10534)
+- ESP8266 (or similar module)
+- Transmitter module (my fireplace remote control uses 433Mhz, and so I got WRL-10534)
 - Voltage Regulator 3.3V
-  - 1000µF lectrolytic Capacitors 
+  - 1000µF Electrolytic Capacitor 
   - 0.1µF Ceramic Capacitor
-  - MCP1700 IC Linear Voltage Regulator 3.3V 250MA
-- 5V DC power supply (via USB or any other means)
+  - MCP1700 IC Linear Voltage Regulator 3.3V 250mA
+- 5V DC power supply (via USB power or something similar)
+
+## Capturing the Remote Code
 
 ## Diagram
 
 ## Build Instructions
 
-## Capturing the code
+## HomeKit Integration
 
 ## Links
--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,24 @@
+/*
+  Fireplace Starter (ESP8266)
+
+  ESP8266 module that sends a remote control signal to start a fireplace
+
+  Created 31 August 2018
+  By Ivan Sy
+  Modified 3 September 2018
+  By Ivan Sy
+
+  https://github.com/isyca/esp8266-fireplace-starter
+*/
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <RCSwitch.h>
 
-RCSwitch mySwitch = RCSwitch();
-ESP8266WebServer server(80);
+RCSwitch txSwitch = RCSwitch();
+ESP8266WebServer webServer(80);
 
 const char* ssid = "SSID";
 const char* password = "password";
@@ -18,46 +31,45 @@ const int pulseLength = 360;
 int lightStatusInt = 0;
 int heatStatusInt = 0;
 
+void turnFirePlaceON ()
+{
+  txSwitch.setRepeatTransmit(2);
+  txSwitch.send("000001110011010111000101");
+  txSwitch.setRepeatTransmit(1);
+  txSwitch.send("000001110011010111");
+  lightStatusInt = 1;
+}
+
+void turnFirePlaceOFF ()
+{
+  txSwitch.setRepeatTransmit(3);
+  txSwitch.send("000001110011010100110101");
+  txSwitch.setRepeatTransmit(1);
+  txSwitch.send("00000");
+  lightStatusInt = 0;
+  heatStatusInt = 0;
+}
+
 void handleRoot() {
-  server.send(200, "application/json", "{\"result\": 0, \"msglevel\": \"ERROR\", \"msg\": \"Access ROOT nothing here\"}");
-}
-
-void turnON ()
-{
-    mySwitch.setRepeatTransmit(2);
-    mySwitch.send("000001110011010111000101");
-    mySwitch.setRepeatTransmit(1);
-    mySwitch.send("000001110011010111");
-    lightStatusInt = 1;
-}
-
-void turnOFF ()
-{
-    mySwitch.setRepeatTransmit(3);
-    mySwitch.send("000001110011010100110101");
-    mySwitch.setRepeatTransmit(1);
-    mySwitch.send("00000");
-    lightStatusInt = 0;
-    heatStatusInt = 0;
+  webServer.send(200, "application/json", "{\"result\": 0, \"msglevel\": \"ERROR\", \"msg\": \"Access ROOT nothing here\"}");
 }
 
 void handleNotFound() {
-  String message = "File Not Found == ";
-  message += "URI: [";
-  message += server.uri();
+  String message = "File Not Found = ";
+  message += " URI: [";
+  message += webServer.uri();
   message += "] Method: [";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += (webServer.method() == HTTP_GET) ? "GET" : "POST";
   message += "] Arguments: [";
-  message += server.args();
+  message += webServer.args();
   message += "]";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += "(" + server.argName(i) + ": " + server.arg(i) + ")";
+  for (uint8_t i = 0; i < webServer.args(); i++) {
+    message += "(" + webServer.argName(i) + ": " + webServer.arg(i) + ")";
   }
-  server.send(404, "application/json", "{\"result\": 0, \"msglevel\": \"ERROR\", \"msg\": \"" + message + "\"}");
+  webServer.send(404, "application/json", "{\"result\": 0, \"msglevel\": \"ERROR\", \"msg\": \"" + message + "\"}");
 }
 
-//count = 5 means approx 1 second delay
-void blinkExternalLED(int count, int remainHigh) 
+void blinkExternalLED(int count, int remainHigh) //count = 5 means approx 1 second delay
 {
   while (count > 0)
   {
@@ -82,64 +94,64 @@ void setup(void) {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  mySwitch.enableTransmit(transmitPin);
-  mySwitch.setRepeatTransmit(2);
-  mySwitch.setPulseLength(pulseLength);
+  txSwitch.enableTransmit(transmitPin);
+  txSwitch.setRepeatTransmit(2);
+  txSwitch.setPulseLength(pulseLength);
 
   while (WiFi.status() != WL_CONNECTED) {    
-    blinkExternalLED(25,0);   // ~5 seconds delay
+    blinkExternalLED(25,0);   // add a ~5 seconds delay
   }
-  server.on("/", handleRoot);
 
-  server.on("/on", []() {
-    blinkExternalLED(5,1);
-    turnON();
-    server.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace on\"}");
+  webServer.on("/", handleRoot);
+
+  webServer.on("/on", []() {
+    blinkExternalLED(2,1);
+    turnFirePlaceON();
+    webServer.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace on\"}");
   });
 
-  server.on("/off", []() {
-    blinkExternalLED(5,1);
-    turnOFF();
-    server.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace off\"}");
+  webServer.on("/off", []() {
+    blinkExternalLED(2,1);
+    turnFirePlaceOFF();
+    webServer.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace off\"}");
   });
 
-  server.on("/heaton", []() {
-    // off, turn on, then turn on again
-    blinkExternalLED(5,1);
-    turnOFF();
-    turnON();
-    turnON();  // 2nd turn on will actually do the heat
+  webServer.on("/heaton", []() {       // off, turn on, then turn on again
+    blinkExternalLED(2,1);
+    turnFirePlaceOFF();
+    turnFirePlaceON();
+    turnFirePlaceON();
     heatStatusInt = 1;
-    server.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace Heat on\"}");
+    webServer.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace Heat on\"}");
   });
 
- server.on("/heatoff", []() {
-    blinkExternalLED(5,1);
-    turnOFF();
-    server.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace Heat off\"}");
+  webServer.on("/heatoff", []() {
+    blinkExternalLED(2,1);
+    turnFirePlaceOFF();
+    webServer.send(200, "application/json", "{\"result\": 1, \"msglevel\": \"INFO\", \"msg\": \"Fireplace Heat off\"}");
   });
 
-  server.on("/status", []() {   
+  webServer.on("/status", []() {   
     String statusStr = String("{\"light\": " + String(lightStatusInt) + ", \"heat\":" + String(heatStatusInt) + "}");
-    server.send(200, "application/json", statusStr);
-  });
-  
-  server.on("/homebridge-lightstatus", []() {   
-    server.send(200, "text/plain", String(lightStatusInt));
+    webServer.send(200, "application/json", statusStr);
   });
 
-  server.on("/homebridge-heatstatus", []() {   
-    server.send(200, "text/plain", String(heatStatusInt));
+  webServer.on("/homebridge-lightstatus", []() {   
+    webServer.send(200, "text/plain", String(lightStatusInt));
   });
 
+  webServer.on("/homebridge-heatstatus", []() {   
+    webServer.send(200, "text/plain", String(heatStatusInt));
+  });
 
- server.onNotFound(handleNotFound);
- server.begin();
- MDNS.begin("IoTfireplace");
+  webServer.onNotFound(handleNotFound);
 
+  webServer.begin();
+
+  MDNS.begin("esp8266-fireplace-starter");
 }
 
 void loop(void) {
   digitalWrite(externalLED, HIGH);
-  server.handleClient();
+  webServer.handleClient();
 }
